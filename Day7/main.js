222cdbf4292de747e75810d644ec0789158ec12a
Amank-root <70597017+Amank-root@users.noreply.github.com>
2021-05-29 17:28:02 +0530

    Just For Self Improvement

---
+++ Day7/main.js
@@ -0,0 +1,16 @@
+var x  = nne;
+var text;
+
+switch (x) {
+    case secret:
+        var fir = "text is done"
+        
+        break;
+    case nne:
+        var sec = "pgl"
+
+    default:
+        text ="book"
+        break;
+}
+document.getElementsByTagName("div").innerHTML = x;
