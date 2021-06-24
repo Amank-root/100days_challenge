066a39cafd628e7c273000e6cbf08033d81b9708
Amank-root <70597017+Amank-root@users.noreply.github.com>
2021-06-24 17:12:23 +0530

    Add files via upload

---
+++ Day33/js/main.js
@@ -0,0 +1,46 @@
+                    // footer //               
+
+
+var foot =  '<footer class="text-gray-400 bg-gray-900 body-font">'+
+                '<div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">'+
+                    '<a class="flex title-font font-medium items-center md:justify-start justify-center text-white">'+
+                        '<img src="./img/icons8-book-64.png" alt="" />'+
+                        '<span class="ml-3 text-xl">My Data</span>'+
+                    '</a>'+
+                    '<p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">&copy; 2020 My Data —'+
+                        '<a href="https://instagram.com/amankushwaha_28" class="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@AmanKushwaha_28</a>'+
+                    '</p>'+
+                    '<span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">'+
+                        '<a class="text-gray-400 ico" href="https://www.instagram.com/amankushwaha_28">'+
+                            '<img src="https://img.icons8.com/nolan/50/instagram-new.png">'+
+                        '</a>'+
+                        '<a class="ml-3 text-gray-400 ico toltipp" href="#">'+
+                            '<span class="toltipptext">Unable On This Platform!</span>'+
+                            '<img src="https://img.icons8.com/nolan/50/facebook-circled.png">'+
+                        '</a>'+
+                        '<a class="ml-3 text-gray-400 ico" href="https://github.com/Amank-root">'+
+                            '<img src="https://img.icons8.com/nolan/50/github.png">'+
+                        '</a>'+
+                    '</span>'+
+                '</div>'+
+            '</footer>';
+
+
+document.getElementById("foot").innerHTML = foot
+
+
+
+
+var passcode = prompt('Something To Say!!');
+
+if (passcode === 'KinG Of HeLL') {
+  dash = confirm('Your Page Is Loaded On Your Screen.')
+  console.log('Lucifer Here.');
+  console.log(dash)
+  if (dash === false){
+      document.documentElement.remove()
+  }
+}
+else {
+  document.documentElement.remove()
+}
