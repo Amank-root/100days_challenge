b8fc9c4fc5be3160aa864d5a37f59763b371e380
Amank-root <70597017+Amank-root@users.noreply.github.com>
2021-07-18 15:51:11 +0530

    Add files via upload

---
+++ Day57/main.js
@@ -0,0 +1,17 @@
+let show = document.getElementsByClassName('fafu')[0]
+let hide = document.getElementsByClassName('hide')[0]
+
+hide.style.display = 'none';
+
+show.addEventListener('click', () => {
+    if (hide.style.display == 'none') {
+        hide.style.display = 'flex'
+        show.innerHTML = '<i class="fas fa-times"></i>'
+    }
+    else {
+        show.innerHTML = '<i class="fas fa-bars"></i>'
+        console.log('hi')
+        hide.style.display = 'none'
+    }
+})
+
